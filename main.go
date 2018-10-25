b359e54697285a788ef303d4329d112c91811a1d
Josiah Baker <josiahfbaker@gmail.com>
2018-10-24 22:03:33 -0700

    return markers as json
    mock data json markers and return them at the `breweries` endpoint

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+// lil marker
+type Marker struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+// get the port from the ENV on startup
 func getPort() (string, error) {
 	port := os.Getenv("PORT")
 	if port == "" {
 		return "", fmt.Errorf("$PORT not set")
 	}
 	return ":" + port, nil
+}
+
+// get markers based on an id
+func getMarkers(id int) []Marker {
+	// mock data for testing
+	markers := []Marker{
+		{Lat: 47.608013, Long: -122.335167},
+		{Lat: 37.5483, Long: -121.9886},
+	}
+	return markers
+}
+
+// return markers when a new request comes in
+func markerHandler(w http.ResponseWriter, r *http.Request) {
+	markers := getMarkers(123)
+	json.NewEncoder(w).Encode(markers)
 }
 
 func main() {
@@ -23,16 +47,10 @@ func main() {
 	}
 	log.Printf("listening on %s...", port[1:])
 
-	//hello world example to get started
+	// return an array of markers
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var response string
-		if r.URL.Path == "/" {
-			response = "hello, world!"
-		} else {
-			response = r.URL.Path[1:]
-		}
-		fmt.Fprintf(w, "tapsie says: %s\n", response)
+		fmt.Fprintf(w, "hello, world!")
 	})
-
+	http.HandleFunc("/breweries", markerHandler)
 	http.ListenAndServe(port, nil)
 }
